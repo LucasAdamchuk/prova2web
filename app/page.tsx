84a9a19368d4faacad47072810d695cd341c65c3
LucasAdamchuk <123332542+LucasAdamchuk@users.noreply.github.com>
2024-06-19 00:17:30 +0000

    corrigindo erro

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-import Banner from "./components/Banner";
-import Course from "./components/Course";
-import Header from "./components/Header";
-import Footer from "./components/Footer"
 
 export default function Home() {
   return (
     <div className="bg-[#3d3d3d] text-white text-center">
-       <Header/>
-       <Banner/>
-       <Course/>
-       <Footer/>
+      <a href="/admin">DMINISTRATIVO</a>
     </div>
   );
 }
